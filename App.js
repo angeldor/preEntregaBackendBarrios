58b2ca08733d3d80a26650e0c1800032c3547cab
angeldor <angelefrenbarriosromero234@gmail.com>
2024-01-15 20:53:22 -0300

    routes

--- App.js
+++ App.js
@@ -1,40 +1,42 @@
-const productManager = require("./ProductManager");
+const productManager = require("./src/ProductManager");
+import "./src/ProductManager";
 const express = require("express");
 const app = express();
 
-app.get("/ping", (req, res) => {
-  res.send("pong");
-});
+app.use(express.json());
+// app.get("/ping", (req, res) => {
+//   res.send("pong");
+// });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
 
-app.get("/products", (req, res) => {
-  //Entrada de datos
-  let productos = productManager.getProducts();
+// app.get("/products", (req, res) => {
+//   //Entrada de datos
+//   let productos = productManager.getProducts();
 
-  const limit = req.query.limit;
+//   const limit = req.query.limit;
 
-  if (limit) {
-    productos = productos.slice(0, parseInt(limit, 10));
-  }
-  //Salida de datos
-  res.send(productos);
-});
+//   if (limit) {
+//     productos = productos.slice(0, parseInt(limit, 10));
+//   }
+//   //Salida de datos
+//   res.send(productos);
+// });
 
-app.get("/products/:id", (req, res) => {
-  const productId = req.params.id;
+// app.get("/products/:id", (req, res) => {
+//   const productId = req.params.id;
 
-  const product = productManager.getProductById(productId);
+//   const product = productManager.getProductById(productId);
 
-  // res.send(product)
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send("Error 404: Producto no encontrado");
-  }
-});
+//   // res.send(product)
+//   if (product) {
+//     res.send(product);
+//   } else {
+//     res.status(404).send("Error 404: Producto no encontrado");
+//   }
+// });
 
 app.listen(8080, () => {
   console.log("Aplicación funcionando en el puerto 8080");
